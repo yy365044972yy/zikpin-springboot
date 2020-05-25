e95b69d11ef34d80a2f51d48a685d16cf25e06f5
wentao <365044972@qq.com>
2020-05-25 16:17:52 +0800

    Initial commit

---
+++ src/main/java/com/wentao/App.java
@@ -0,0 +1,19 @@
+package com.wentao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 李文涛
+ * @title: App
+ * @description: tao微服务电商项目
+ * @teacher 蚂蚁课堂创始人-余胜军QQ644064779
+ * date 2020/3/1617:24
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
+}
---
+++ src/main/java/com/wentao/controller/demoController.java
@@ -0,0 +1,27 @@
+package com.wentao.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 李文涛
+ * @title: demoController
+ * @description: tao微服务电商项目
+ * @teacher 蚂蚁课堂创始人-余胜军QQ644064779
+ * date 2020/3/1617:20
+ */
+
+@Controller
+public class demoController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
